24756812c438651907347e83b3a33fb586587ad2
Fabio <fabio@fabiofarias.com.br>
2020-10-08 17:35:48 -0300

    Deploy Again

--- src/controllers/HomeController.ts
+++ src/controllers/HomeController.ts
@@ -4,7 +4,7 @@ export default class HomeController {
     async index(request: Request, response: Response) {
         try {
             const enviromentTest = process.env.TESTE_ENV;
-            return response.send({message: 'Hello', TESTE_ENV: enviromentTest});
+            return response.send({message: 'Hello Deploy Again', TESTE_ENV: enviromentTest});
         } catch (err) {
             return response.status(400).json({
                 error: 'Erro index class'
